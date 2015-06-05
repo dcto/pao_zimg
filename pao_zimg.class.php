dc3fab2a066d17c9576af0d0da7158bbe2737917
D.C <dc@weststarinc.co>
2015-06-06 01:52:36 +0800

    init

---
+++ pao_zimg.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ *	[[泡]Zimg图片附件服务器(pao_zimg.{modulename})] (C)2015-2099 Powered by Copyright @ Pao11.com Design by 陶之11(Pao11).
+ *	Version: v1
+ *	Date: 2015-6-6 01:25
+ */
+
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+class plugin_pao_zimg {
+	//TODO - Insert your code here
+
+}
+
+?>
